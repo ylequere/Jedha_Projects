809b39dec8b243405d2b96057075e190da58018e
ylequere <ylequere@gmail.com>
2023-03-11 09:12:51 +0100

    Adding insights aside the map.

--- Bloc6-FinalProject/Website/app.py
+++ Bloc6-FinalProject/Website/app.py
@@ -31,64 +31,34 @@ custom_styles= """
 .main > .block-container {
     padding-top: 2rem;
 }
+
+.main > footer {
+    display: none;
+}
 </style>
 """
 st.markdown(custom_styles, unsafe_allow_html=True)
 
-# hide_streamlit_style = """
-#             <style>
-#             #MainMenu {visibility: visible}
-#             footer {visibility: hidden;}
-#             header {visibility: visible;}
-#             </style>
-#             """
-# st.markdown(hide_streamlit_style, unsafe_allow_html=True)
-
-## Hack to reduce space up screen
-# st.markdown("""
-#         <style>
-#                 .css-k1ih3n {
-#                     padding-top: 1rem;
-#                     padding-bottom: 10rem;
-#                     padding-left: 5rem;
-#                     padding-right: 5rem;
-#                 }
-#                 .css-1d391kg {
-#                     padding-top: 3.5rem;
-#                     padding-right: 1rem;
-#                     padding-bottom: 3.5rem;
-#                     padding-left: 1rem;
-#                 }
-#                 .css-1vq4p4l {
-#                     padding-top: 1rem;
-#                     padding-right: 1rem;
-#                     padding-bottom: 1.5rem;
-#                     padding-left: 1rem;
-#                     }
-#         </style>
-#         """, unsafe_allow_html=True)
-
 @st.cache_data
 def get_full_dataset():
-    return pd.read_csv(sub_folder + 'data/RealEstate_PARIS_FR_2022.csv')
+    return pd.read_csv(sub_folder + 'data/TempFiles/RealEstate_PARIS_FR_2022.csv')
 
 model = XGBRegressor()
 model.load_model(sub_folder + 'data/RealEstate_PARIS_FR_2022.xgbmodel')
 preprocessor = load(open(sub_folder + 'data/preprocessor.dmp', 'rb'))
 df_full_dataset = get_full_dataset()
 
-nature_mutation = st.sidebar.selectbox('Type de vente', ('Vente', "Vente en l'état futur d'achèvement", 'Vente terrain à bâtir'))
-type_local = st.sidebar.selectbox('Type de bien', ('Appartement', 'Maison'))
+nature_mutation = st.sidebar.selectbox('Type de vente', ('Vente', "Vente en l'état futur d'achèvement"))
+type_local = 'Appartement'
 nb_pieces = st.sidebar.slider('Nombre de pièces', 1, 8, 2, 1)
 surface = st.sidebar.slider('Surface m²', 10, 260, 80, 2)
 adresse = st.sidebar.text_input('Adresse', '50 avenue des champs elysees PARIS FRANCE')
-
 
 map_style = 'carto-positron'
 # st.header("ESTIMATION D'UN BIEN IMMOBILIER A PARIS INTRAMUROS")
 start_clicked = st.sidebar.button('👉 Calculer estimation 👈', type="primary", use_container_width=True)
 if start_clicked:
-    loc = Nominatim(user_agent="MyAppRE75").geocode(adresse)
+    loc = Nominatim(user_agent="MyAppRE75").geocode(adresse, language="fr")
     compute = (loc is not None) and ('PARIS' in adresse.upper())
     if compute:
         df_real_test = pd.DataFrame({'Nature mutation':[nature_mutation]
@@ -110,6 +80,7 @@ if start_clicked:
 
         # Filtering sales around the lat/lon of desired real-estate
         df_full_dataset = df_full_dataset[(abs(df_full_dataset['Latitude'] - loc.point[0]) < 0.005) &  (abs(df_full_dataset['Longitude'] - loc.point[1]) < 0.01)]
+        df_full_dataset['Valeur fonciere raw'] = df_full_dataset['Valeur fonciere']
         df_full_dataset['Valeur fonciere'] = df_full_dataset['Valeur fonciere'].apply(lambda x : '{:,} €'.format(round(x)).replace(',', ' '))
         df_full_dataset['Surface_m'] = df_full_dataset['Surface']
         df_full_dataset['Surface'] = df_full_dataset['Surface'].apply(lambda x : '{:.0f} m²'.format(x))
@@ -138,10 +109,41 @@ if start_clicked:
         fig.add_trace(fig2.data[0])
         fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 
-        estimate = "Le bien est estimé à {:,} €".format(round(Y_real_test[0])).replace(',', ' ')
-        st.header(estimate)
+        estimate_raw = round(Y_real_test[0])
+        estimate = "{:,} €".format(estimate_raw).replace(',', ' ')
+        st.header("Le bien est estimé à " + estimate)
 
-        st.plotly_chart(fig)
+        col1, col2 = st.columns([4, 1])
+
+        with col1:
+            st.plotly_chart(fig, use_container_width=True)
+
+        with col2:
+            st.subheader('Secteur')
+
+            secteur_valeur_fonciere_mean = df_full_dataset['Valeur fonciere raw'].mean()
+            valeur_fonciere_delta = estimate_raw - secteur_valeur_fonciere_mean
+
+            st.metric(label="Valeur foncière moyenne"
+                    , value="{:,} €".format(round(secteur_valeur_fonciere_mean)).replace(',', ' ')
+                    , delta="{:,} €".format(round(valeur_fonciere_delta)).replace(',', ' ')
+                    )
+
+            secteur_surface_m_mean = df_full_dataset['Surface_m'].mean()
+            surface_m_delta = surface - secteur_surface_m_mean
+
+            st.metric(label="Surface moyenne"
+                      , value='{:.00f} m²'.format(round(secteur_surface_m_mean, 2))
+                      , delta='{:.00f} m²'.format(round(surface_m_delta, 2))
+                      )
+
+            secteur_nb_pieces_mean = df_full_dataset['Nombre pieces principales'].mean()
+            nb_pieces_delta = nb_pieces - secteur_nb_pieces_mean
+
+            st.metric(label="Nb moyen de pièces"
+                      , value=round(secteur_nb_pieces_mean, 2)
+                      , delta=round(nb_pieces_delta, 2)
+                      )
 
     else:
         st.header("Géolocalisation impossible, veuillez indiquer une autre adresse.")
